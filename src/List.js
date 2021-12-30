aafbb5c6c305f7211c96c3727693cd67e338c457
SchillingCodes <tamakuchiko@gmail.com>
2021-12-30 10:56:08 -0500

    add CRUD functionality. Some styling.

--- src/List.js
+++ src/List.js
@@ -1,11 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ListItem from './ListItem';
+import { collection, addDoc, getDocs, deleteDoc, doc, query, where } from "firebase/firestore";
 
 function List(props) {
-  // Declare a new state variable, which we'll call "count"
     const [tasks, setTasks] = useState([]);
-
-    const [i, setI] = useState(0);
+    useEffect(() => {
+        fetchTasks();
+    }, []);
     
     const [input, setInput] = useState("");
 
@@ -13,13 +14,36 @@ function List(props) {
         setInput(e.target.value);
     }
 
-    function addItem() {
-        setTasks(state => [...state, {id: "todo-" + i, name: input, completed: false}]);
-        setI(i + 1);
+    async function addItem() {
+        try {
+            const docRef = await addDoc(collection(props.firebase.db, "todo"), {
+                task: input
+            });
+            console.log("Document written with ID: ", docRef.id);
+            setTasks((tasks) => [...tasks, {id: input, name: input}])
+        } catch (e) {
+            console.error("Error adding document: ", e);
+        }
     }
 
-    function deleteItem(id) {
-        setTasks(tasks.filter(item => item.id !== id));
+    async function deleteItem(id) {
+        try {
+            const docRef = collection(props.firebase.db, "todo");
+
+            // Create a query against the collection.
+            const q = query(docRef, where("task", "==", id));
+            const querySnapshot = await getDocs(q);
+            querySnapshot.forEach((d) => {
+                deleteDoc(doc(props.firebase.db, "todo", d.id));
+            });
+            setTasks((tasks) => (tasks.filter((task) => {
+                console.log(task.id);
+                console.log(id);
+                return task.id !== id;
+            })));
+        } catch (e) {
+            console.error("Error deleting document: ", e);
+        }
     }
 
     const taskList = tasks.map(task => {
@@ -32,11 +56,25 @@ function List(props) {
         );
     });
 
+    async function fetchTasks() {
+        try {
+            const querySnapshot = await getDocs(collection(props.firebase.db, "todo"));
+            querySnapshot.forEach((d) => {
+                // d.data() is never undefined for query doc snapshots
+                setTasks((tasks) => ([...tasks, {id: d.data()["task"], name: d.data()["task"]}]));
+            });
+        } catch (e) {
+            console.error("Error reading data: ", e);
+        }
+    }
+
     return (
-        <div>
+        <div className="listContainer">
             <div>{taskList}</div>
-            <input id="newItemInput" onChange={handleChange}></input>
-            <button onClick={addItem}>Add Item</button>
+            <div className="itemList">
+                <input id="newItemInput" onChange={handleChange} maxLength="10"></input>
+                <button className="btn" onClick={addItem}>Add Item</button>
+            </div>
         </div>
     );
 }
